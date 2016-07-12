f0620a7c53f9d9366e2ab9258f98c47e9a4b2197
Shigeaki Nishina <shgaki@gmail.com>
2016-07-12 11:01:51 +0900

    Replace deprecated NSOpenPanel methods

--- JournalTabController.m
+++ JournalTabController.m
@@ -3708,7 +3708,7 @@ bail:
 		else
 		{
 			NSInteger j;
-			NSArray *files = [oPanel filenames];
+			NSArray *files = [oPanel URLs];
 			
 			// add the files to the entry
 			for ( j = 0; j < [files count]; j++ ) {
